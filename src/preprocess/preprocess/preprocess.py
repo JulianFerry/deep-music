6e95c4c4f75d0c60cf13af179ba652acc50f8643
Julian Ferry <julianferry94@gmail.com>
2020-08-07 18:21:20 +0000

    Converted preprocess to Apache Beam pipeline - works locally with DirectRunner

---
+++ src/preprocess/preprocess/preprocess.py
@@ -0,0 +1,60 @@
+from scipy.io import wavfile
+from audiolib import Audio
+
+import apache_beam as beam
+
+
+def load_audio(
+    f: beam.io.fileio.ReadableFile
+):
+    """
+    """
+    file_name = f.metadata.path.split('/')[-1]
+    sampling_rate, data = wavfile.read(f.open())
+    audio = Audio(data, sampling_rate)
+    return file_name, audio
+
+
+class PreProcessor:
+
+    def __init__(self, fft_params=None):
+        """
+        Initialise the preprocessor with the parameters used to
+        transform the audio
+
+        Parameters
+        ----------
+        fft_params: dict
+            Dictionary with optional keys
+            - 'start', 'end' (float: default 0, -1):
+              audio start and end time cutoffs in seconds
+            - 'time_intervals', 'resolution' (default 1 and 5):
+              FFT parameters see audiolib.Audio.to_spectrogram for details
+
+        """
+        self.fft_params = {} if fft_params is None else fft_params
+        default_params = {
+            'time_intervals': 1,
+            'resolution': 5,
+            'start': 0,
+            'end': -1,
+            'config': []
+        }
+        for k, v in default_params.items():
+            self.fft_params[k] = self.fft_params.get(k, v)
+
+    def audio_to_spectrogram(self, record):
+        """
+        Convert an audio object to a spectrogram given a set of fft_params
+
+        """
+        file_name, audio = record
+        spec = (audio
+            .trim(self.fft_params['start'], self.fft_params['end'])
+            .to_spectrogram(
+                self.fft_params['time_intervals'],
+                self.fft_params['resolution'],
+                cqt=True
+            ))
+        return file_name, spec
+
